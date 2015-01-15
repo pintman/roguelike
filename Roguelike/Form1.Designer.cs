331e5e1d75b7728dc6107576843a16b628a242cf
Marco Bakera <Marco@Bakera.de>
2015-01-15 19:14:57 +0100

    erste Version.

---
+++ Roguelike/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Roguelike
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbSpielfeld = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spielfeldLadenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtbSpielfeld
+            // 
+            this.rtbSpielfeld.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbSpielfeld.Location = new System.Drawing.Point(13, 27);
+            this.rtbSpielfeld.Name = "rtbSpielfeld";
+            this.rtbSpielfeld.Size = new System.Drawing.Size(503, 396);
+            this.rtbSpielfeld.TabIndex = 0;
+            this.rtbSpielfeld.Text = "";
+            this.rtbSpielfeld.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.rtbSpielfeld_KeyPress);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dateiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(528, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dateiToolStripMenuItem
+            // 
+            this.dateiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.spielfeldLadenToolStripMenuItem});
+            this.dateiToolStripMenuItem.Name = "dateiToolStripMenuItem";
+            this.dateiToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.dateiToolStripMenuItem.Text = "Datei";
+            // 
+            // spielfeldLadenToolStripMenuItem
+            // 
+            this.spielfeldLadenToolStripMenuItem.Name = "spielfeldLadenToolStripMenuItem";
+            this.spielfeldLadenToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.spielfeldLadenToolStripMenuItem.Text = "Spielfeld laden";
+            this.spielfeldLadenToolStripMenuItem.Click += new System.EventHandler(this.spielfeldLadenToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 435);
+            this.Controls.Add(this.rtbSpielfeld);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbSpielfeld;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dateiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spielfeldLadenToolStripMenuItem;
+    }
+}
+
---
+++ Roguelike/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace Roguelike
+{
+    public partial class Form1 : Form
+    {
+        int breite;
+        int hoehe;
+        char[,] aSpielfeld;
+
+        int iSpielerX;
+        int iSpielerY;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            breite = 15;
+            hoehe = 15;
+            iSpielerX = 5;
+            iSpielerY = 5;
+
+            rtbSpielfeld.Font = new Font("Courier New", 12);
+            rtbSpielfeld.BackColor = Color.Black;
+            rtbSpielfeld.ForeColor = Color.Green;
+
+            SpielfeldInitialisieren();
+            SpielfeldZeichnen();
+        }
+
+        private void SpielfeldInitialisieren()
+        {
+            aSpielfeld = new char[breite, hoehe];
+
+            for (int i = 0; i < breite; i++)
+			{
+                for (int j = 0; j < hoehe; j++)
+                {
+                    aSpielfeld[i,j] = '.';
+                }
+			}
+
+            aSpielfeld[iSpielerX, iSpielerY] = '@';
+
+            aSpielfeld[iSpielerX + 2, iSpielerY] = '#';
+        }
+        private void SpielfeldZeichnen1()
+        {
+            // Variante 1: Nutzt AppendText. Das flackert jedoch. Daher wechsel zu
+
+            rtbSpielfeld.Clear();
+            for (int j = 0; j < hoehe; j++)
+            {
+                for (int i = 0; i < breite; i++)
+                {
+                    char c = aSpielfeld[i, j];
+                    rtbSpielfeld.AppendText(c.ToString());
+                }
+                rtbSpielfeld.AppendText("\n");                
+            }
+        }
+        private void SpielfeldZeichnen()
+        {
+            // Variante 2: Erst in einen String schreiben, 
+            // diesen dann komplett setzen (Buffering)
+
+            string sSpielfeld = "\n";
+            rtbSpielfeld.Clear();
+            for (int j = 0; j < hoehe; j++)
+            {
+                for (int i = 0; i < breite; i++)
+                {
+                    char c = aSpielfeld[i, j];
+                    sSpielfeld += c.ToString();
+                }
+                sSpielfeld += "\n";
+            }
+            rtbSpielfeld.Text = sSpielfeld;
+        }
+
+        private void rtbSpielfeld_KeyPress1(object sender, KeyPressEventArgs e)
+        {
+            // Variante 1: Ohne Prüfung auf Kollision
+            aSpielfeld[iSpielerX, iSpielerY] = '.';
+
+            if(e.KeyChar == 'a')
+            {
+                iSpielerX--;
+            }
+            if(e.KeyChar == 'd')
+            {
+                iSpielerX++;
+            }
+            if (e.KeyChar == 'w')
+            {
+                iSpielerY--;
+            }
+            if (e.KeyChar == 's')
+            {
+                iSpielerY++;
+            }
+
+            aSpielfeld[iSpielerX, iSpielerY] = '@';
+
+            SpielfeldZeichnen();
+        }
+        private void rtbSpielfeld_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Variante 2: Kollisionsprüfung. Nur laufen, wenn Zielfeld leer ist.
+
+            aSpielfeld[iSpielerX, iSpielerY] = '.';
+
+            if (e.KeyChar == 'a' && feldFrei(iSpielerX-1, iSpielerY))
+            {
+                iSpielerX--;
+            }
+            if (e.KeyChar == 'd' && feldFrei(iSpielerX + 1, iSpielerY))
+            {
+                iSpielerX++;
+            }
+            if (e.KeyChar == 'w' && feldFrei(iSpielerX,iSpielerY-1))
+            {
+                iSpielerY--;
+            }
+            if (e.KeyChar == 's' && feldFrei(iSpielerX,iSpielerY+1))
+            {
+                iSpielerY++;
+            }
+
+            aSpielfeld[iSpielerX, iSpielerY] = '@';
+
+            SpielfeldZeichnen();
+        }
+        /// <summary>
+        /// Prüft, ob das Feld frei ist und der Spieler darauf ziehen kann.
+        /// </summary>
+        private bool feldFrei(int x, int y)
+        {
+            if(aSpielfeld[x,y] == '.')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void spielfeldLadenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofdSpielfeldLaden = new OpenFileDialog();
+            DialogResult result = ofdSpielfeldLaden.ShowDialog();
+
+            int iZeile = 0;
+            if(result == DialogResult.OK)
+            {
+                StreamReader reader = new StreamReader(ofdSpielfeldLaden.FileName);
+                while (!reader.EndOfStream)
+                {
+                    string sZeile = reader.ReadLine();
+                    if (sZeile.Length != breite)
+                    {
+                        MessageBox.Show("Zeile hat falsche Länge " + sZeile);
+                    }
+
+                    for (int i = 0; i < sZeile.Length; i++)
+                    {
+                        char c = sZeile[i];
+                        aSpielfeld[iZeile, i] = c;
+                        if(c == '@')
+                        {
+                            iSpielerX = i;
+                            iSpielerY = iZeile;
+                        }
+                    }
+
+                    iZeile++;
+                }
+                SpielfeldZeichnen();
+            }
+        }
+    }
+}
